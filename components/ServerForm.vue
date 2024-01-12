f09fbc5646042bf8bc875710b9e748ed03d3a562
Рустам Курбанбаев <147426350+rustamfront22@users.noreply.github.com>
2024-01-12 13:34:29 +0500

    fix select ios

--- components/ServerForm.vue
+++ components/ServerForm.vue
@@ -20,7 +20,7 @@
     <div class="flex flex-col w-full">
       <label class="text-md font-bold text-gray-500 mb-2">Тип сервера</label>
       <select
-          class="w-full px-4 py-2 mb-4 text-lg text-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-200"
+          class="w-full px-4 py-2 mb-4 text-lg text-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-200 appearance-none bg-[url('/images/arrow-down.svg')] bg-no-repeat bg-right bg-[calc(100%_-_10px)_center] bg-white"
           v-model="state.server_type">
         <option value="vds">VDS</option>
         <option value="dedicated">Выделенный сервер</option>
